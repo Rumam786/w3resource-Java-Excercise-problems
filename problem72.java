c2903c22da4cfd0e1a5018c35ada57b40331fb80
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-09-20 11:50:53 +0500

    java

--- problem72.java
+++ problem72.java
@@ -1,11 +1,24 @@
-// problem 103
+// Write a Java program that accepts four integer from the user and 
+// prints equal if all four are equal, and not equal otherwise.
 
 
 
-
-
+import java.util.Scanner;
 class problem72 {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Type First Number");
+        int a = sc.nextInt();
+        System.out.println("Type Second Number");
+        int b = sc.nextInt();
+        System.out.println("Type Third Number");
+        int c = sc.nextInt();
+        System.out.println("Type Fourth Number");
+        int d = sc.nextInt();
+        if (a == b && a == c && a == d && b == c && b == d && c == d) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Not Equal");
+        }
     }
 }
